655fd455aa3eb2bea6772fa848b647df6189d0bc
ishanyadav13290 <ishanyadav13290@gmail.com>
2023-11-18 14:52:22 +0530

    cors update

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ import userRoutes from "./routes/userRoutes.js";
 import postRoutes from "./routes/postRoutes.js";
 import messageRoutes from "./routes/messageRoutes.js";
 import { v2 as cloudinary } from "cloudinary";
-const cors = require("cors");
+import cors from "cors"
 import { app, server } from "./socket/socket.js";
 
 dotenv.config();
